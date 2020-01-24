5f9ecc57eb759093a4ce2d2ff6058fbee7971e7f
parshva-b <parshva.barbhaya@somaiya.edu>
2020-01-24 16:37:27 +0530

    EJS explained

--- index.js
+++ index.js
@@ -10,9 +10,28 @@ app.set('view engine', 'ejs')
 app.use(cors())
 app.use(bodyParser.json())
 
+const people = [
+    {
+        id: 1,
+        name: 'Parshva'
+    },
+    {
+        id: 2,
+        name: 'Sakina'
+    }
+]
+
 // routers
 app.get('/', ( req, res ) => {
     res.render('index')
+})
+
+app.get('/add', ( req, res ) => {
+    res.render('add', { a: 10, b: 20 })
+})
+
+app.get('/table', (req, res) => {
+    res.render('table', { persons: people })
 })
 
 const PORT = process.env.PORT
--- readme.md
+++ readme.md
@@ -1,4 +1,19 @@
+To run:
+```
+node v12.14.1
+yarn 1.21.1
+```
+
+To set up envirnoment
+```
+cd to this directory
+yarn install
+yarn watch
+```
+
 Add any package:
-> yarn add [package] (-D {Dev Dependency})
+```
+yarn add [package] (-D {Dev Dependency})
+```
 
 For Ejs syntax use index.js as template
